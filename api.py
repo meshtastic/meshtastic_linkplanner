c52287b1e676fffdff45c79bcdfb63389578f4f8
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2024-09-17 00:07:02 -0600

    initial commit

---
+++ api.py
@@ -0,0 +1,119 @@
+import json
+
+from fastapi import FastAPI, Request, HTTPException, Depends, Header
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
+from geoprop import Tiles, Itm, Point, Climate
+from dotenv import dotenv_values
+from regions import meshtastic_regions
+import logging
+import h3
+import sys
+
+logging.basicConfig(level=logging.INFO)
+
+config = dotenv_values(".env")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your frontend's domain if needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # Allows all headers (x-api-key, Content-Type, etc.)
+)
+
+def load_config() -> dict:
+    try:
+        config = dotenv_values(".env")
+        tile_dir = config["SRTM_TILE_DIR"]
+        h3_res = int(config["MODEL_H3_RES"])
+        max_distance_km = float(config["MAX_DISTANCE_KM"])
+        api_key = config["API_KEY"]
+        return {
+            "tile_dir": tile_dir,
+            "h3_res": h3_res,
+            "max_distance_km": max_distance_km,
+            "api_key": api_key,
+        }
+    except KeyError as e:
+        logging.error(f"{e} not found in .env file, exiting.")
+        sys.exit(1)
+    except ValueError as e:
+        logging.error(f"Invalid value for {e}, exiting.")
+        sys.exit(1)
+
+
+config = load_config()
+tiles = Tiles(config["tile_dir"])
+itm = Itm(tiles, climate=Climate.ContinentalTemperate)
+
+
+class PredictRequest(BaseModel):
+    lat: float = Field(..., ge=-90, le=90, description="Latitude")
+    lon: float = Field(..., ge=-180, le=180, description="Longitude")
+    txh: float = Field(1.0, gt=0, description="Transmission height in meters")
+    rxh: float = Field(1.0, gt=0, description="Reception height in meters")
+    tx_gain: float = Field(1.0, ge=0, description="Transmission gain in dB")
+    rx_gain: float = Field(1.0, ge=0, description="Reception gain in dB")
+    region: str = Field("US", description="Region code")
+
+
+async def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != config["api_key"]:
+        raise HTTPException(status_code=401, detail="Invalid or missing API key")
+
+
+@app.post("/predict", dependencies=[Depends(verify_api_key)])
+async def predict(payload: PredictRequest) -> dict:
+
+    if payload.region not in meshtastic_regions.keys():
+        raise HTTPException(
+            status_code=404, detail=f"Region '{payload.region}' not found"
+        )
+
+    try:
+        center = Point(payload.lat, payload.lon, payload.txh)
+        coverage = itm.coverage(
+            center,
+            config["h3_res"],
+            meshtastic_regions[payload.region]["frequency"] * 1e6,
+            config["max_distance_km"],
+            payload.rxh,
+            rx_threshold_db=None,
+        )
+
+        print(coverage)
+    except ValueError as e:
+        logging.error(f"Coverage calculation error: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Error generating model prediction: {str(e)}"
+        )
+
+    # coverage is a list of [h3 cell, elevation in meters, loss in dB]
+    # convert to a list of model RSSI as a function of lat, lon
+
+    tx_power = meshtastic_regions[payload.region]["transmit_power"]
+
+    geojson_output = {"type": "FeatureCollection", "features": []}
+
+    for row in coverage:
+        center_h3, elevation, loss_db = row
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": h3.h3_to_geo(hex(center_h3)),  # latitude, longitude
+            },
+            "properties": {
+                "elevation": elevation,
+                "rssi": tx_power + payload.tx_gain + payload.rx_gain - loss_db,
+            },
+        }
+        geojson_output["features"].append(feature)
+    print(geojson_output)
+    with open("test.json", "w") as f:
+        f.write(json.dumps(geojson_output))
+        f.close()
+    return geojson_output
---
+++ regions.py
@@ -0,0 +1,19 @@
+meshtastic_regions = {
+    "EU_868": {"frequency": 868.3, "transmit_power": 14},
+    "EU_433": {"frequency": 433.3, "transmit_power": 12},
+    "ANZ": {"frequency": 915.2, "transmit_power": 27},
+    "US": {"frequency": 903.0, "transmit_power": 30},
+    "CN": {"frequency": 470.5, "transmit_power": 20},
+    "IN": {"frequency": 865.4, "transmit_power": 30},
+    "JP": {"frequency": 920.8, "transmit_power": 16},
+    "KR": {"frequency": 922.3, "transmit_power": 23},
+    "MY_919": {"frequency": 920.0, "transmit_power": 14},
+    "MY_433": {"frequency": 433.3, "transmit_power": 12},
+    "NZ_865": {"frequency": 865.4, "transmit_power": 30},
+    "RU": {"frequency": 864.2, "transmit_power": 30},
+    "SG_923": {"frequency": 923.4, "transmit_power": 16},
+    "TW": {"frequency": 922.3, "transmit_power": 30},
+    "TH": {"frequency": 921.0, "transmit_power": 30},
+    "UA_868": {"frequency": 868.3, "transmit_power": 14},
+    "UA_433": {"frequency": 433.3, "transmit_power": 12}
+}
