a2cb0cfd00809738932c96307b9ba338067e3de7
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2024-09-18 22:36:30 -0600

    https required for cors

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://meshplanner.mpatrick.dev"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
